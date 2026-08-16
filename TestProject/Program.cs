5709b03899fd247cced7bb0415ad9e3a8dc05296
OMI786I <sohrawardy1998@gmail.com>
2026-08-17 00:27:39 +0600

    trying different class and methods

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
+
+string name = "omi";
+name = name.ToUpper();
+Console.WriteLine(name);
+
+Random random = new Random();
+Console.WriteLine(random.Next(1, 100));
