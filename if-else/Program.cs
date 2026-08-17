1502ea3c02a1e6d0e02ff7d9c5fcebaefafa7705
OMI786I <sohrawardy1998@gmail.com>
2026-08-17 22:28:53 +0600

    getting started with if-else

---
+++ if-else/Program.cs
@@ -0,0 +1,19 @@
+﻿Random dice = new ();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine(total);
+
+if (total >= 18)
+{
+    Console.WriteLine("You win bro");
+}
+
+if (total < 18)
+{
+    Console.WriteLine("You are a minor bro");
+}
