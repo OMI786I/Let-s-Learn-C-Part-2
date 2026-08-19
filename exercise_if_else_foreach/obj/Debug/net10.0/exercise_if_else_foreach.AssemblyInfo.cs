046418050c43b6dacae42dffccfde1b16f645c36
OMI786I <sohrawardy1998@gmail.com>
2026-08-19 21:57:05 +0600

    added letter grade

--- exercise_if_else_foreach/obj/Debug/net10.0/exercise_if_else_foreach.AssemblyInfo.cs
+++ exercise_if_else_foreach/obj/Debug/net10.0/exercise_if_else_foreach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise_if_else_foreach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bd5edcfdddb1d5e214e3b96cbb2a25162e7edd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468c27cbb256e01e3c571ac5e975aa690d5ab21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise_if_else_foreach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise_if_else_foreach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
